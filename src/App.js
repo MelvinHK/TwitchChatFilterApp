f81bc0d7d6585e35abb8df546ba0ed344e95d818
Melvin <melvinhkusuma@gmail.com>
2023-03-07 19:28:36 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import tmi from 'tmi.js';
 
 function App() {
+  const chatbox = useRef(null);
+  const [name, setName] = useState('');
+  const [client, setClient] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    chatbox.current.innerHTML = '';
+    setClient(new tmi.Client({
+      channels: [name]
+    }));
+  }
+
+  useEffect(() => {
+    if (!client)
+      return;
+
+    client.connect();
+
+    const handleNewMessage = (channel, tags, message, self) => {
+      const newMessage = document.createElement('li');
+      newMessage.setAttribute('id', tags.id)
+      console.log(tags.color)
+      newMessage.innerHTML = `<span style='color:${tags.color};'>${tags['display-name']}</span>: ${message}`;
+      chatbox.current.insertBefore(newMessage, chatbox.current.firstChild);
+    }
+
+    client.on('message', handleNewMessage);
+
+    return () => {
+      client.disconnect();
+      client.off('message', handleNewMessage)
+    }
+  }, [client]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex-container'>
+      <div className='util-column'>
+        <form onSubmit={handleSubmit}>
+          <h3>Twitch Chat Relay</h3>
+          <div className='flex-container'>
+            <input type='text' value={name} onChange={(e) => setName(e.target.value)}
+              placeholder='Channel Name' className='channel-search' />
+            <input type='submit' className='submit-btn' />
+          </div>
+        </form>
+      </div>
+      <ul ref={chatbox} className='chatbox' />
     </div>
   );
 }
