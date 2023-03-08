47ab3c534b31982bdf648ea473c3a7db1e48f9b7
Melvin <melvinhkusuma@gmail.com>
2023-03-08 16:53:04 +1100

    implemented scroll to bottom button

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ function App() {
   const chatbox = useRef(null);
   const [name, setName] = useState('');
   const [client, setClient] = useState();
+  const [scrolledBottom, setScrolledBottom] = useState(true)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    chatbox.current.innerHTML = '';
+    chatbox.current.replaceChildren();
     setClient(new tmi.Client({
       channels: [name]
     }));
@@ -21,21 +22,41 @@ function App() {
 
     client.connect();
 
+    return () => {
+      client.disconnect();
+    }
+  }, [client]);
+
+  useEffect(() => {
+    if (!client)
+      return;
+
     const handleNewMessage = (channel, tags, message, self) => {
       const newMessage = document.createElement('li');
-      newMessage.setAttribute('id', tags.id)
-      console.log(tags.color)
+      newMessage.setAttribute('id', tags.id);
       newMessage.innerHTML = `<span style='color:${tags.color};'>${tags['display-name']}</span>: ${message}`;
-      chatbox.current.insertBefore(newMessage, chatbox.current.firstChild);
+      chatbox.current.appendChild(newMessage);
+      if (scrolledBottom)
+        chatbox.current.scrollTop = chatbox.current.scrollHeight;
     }
 
     client.on('message', handleNewMessage);
 
     return () => {
-      client.disconnect();
-      client.off('message', handleNewMessage)
+      client.off('message', handleNewMessage);
     }
-  }, [client]);
+  }, [client, scrolledBottom])
+
+  const handleScroll = (e) => {
+    if (Math.abs(e.scrollHeight - e.clientHeight - e.scrollTop) < 1)
+      setScrolledBottom(true)
+    else
+      setScrolledBottom(false)
+  }
+
+  const scrollToBottom = () => {
+    chatbox.current.scrollTop = chatbox.current.scrollHeight;
+  }
 
   return (
     <div className='flex-container'>
@@ -45,11 +66,19 @@ function App() {
           <div className='flex-container'>
             <input type='text' value={name} onChange={(e) => setName(e.target.value)}
               placeholder='Channel Name' className='channel-search' />
-            <input type='submit' className='submit-btn' />
+            <input type='submit' className='btn submit-btn' />
           </div>
         </form>
       </div>
-      <ul ref={chatbox} className='chatbox' />
+      <div className='chatbox-div'>
+        <ul ref={chatbox} className='chatbox' onScroll={(e) => { handleScroll(e.target) }} />
+        <button
+          className={`btn scroll-bottom-btn ${scrolledBottom ? 'd-none' : ''}`}
+          onClick={() => scrollToBottom()}
+        >
+          {'\u{1F847}'} Scroll to bottom
+        </button>
+      </div>
     </div>
   );
 }
